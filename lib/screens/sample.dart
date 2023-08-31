8650497167a1aafcfdfe2c8146ab9c0692e0815c
jasviram.s <jasviram.s@marketsimplified.com>
2023-08-31 18:12:48 +0530

    - MaterialPage route - Static Route - Dynamic Route added

---
+++ lib/screens/sample.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'dynamic_screen.dart';
+
+//test file for sample use 
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              DynamicScreenTest.routeName,
+              arguments: 'From Home Screen',
+            );
+          },
+          child: const Text('Go to Dynamic test screen'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/screens_container.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import 'dynamic_screen.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen one'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to Screen two'),
+          onPressed: () {
+            Navigator.pushNamed(context, "/second");
+            // print( Navigator.canPop(context));
+            // Navigator.pushReplacement(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => const Screen2(name: "Screen two"),
+            //   ),
+            // );
+            // Navigator.push(
+            //   context,
+            //   // MaterialPageRoute(builder: (context) =>const Screen2(name:"Screen two")),
+            // );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({super.key, required this.name});
+
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const Screen3(),
+                  settings: const RouteSettings(arguments: "Arg pass test")),
+            );
+          },
+          child: const Text('Go to screen 3'),
+        ),
+      ),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  const Screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final String val = ModalRoute.of(context)!.settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen Three"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.maybePop(context);
+              },
+              child: const Text('Go back'),
+            ),
+            Text(val),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  DynamicScreenTest.routeName,
+                  arguments: "Dynamic screen argument"
+                );
+              },
+              child: const Text('Go to dynamic screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
