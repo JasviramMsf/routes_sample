0757b110f598fd66ee7e2f7fe1422bea36779165
jasviram.s <jasviram.s@marketsimplified.com>
2023-09-07 12:39:46 +0530

    removed unecessary codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:routes/screens/dynamic_screen.dart';
-import 'package:routes/screens/screens_container.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,41 +9,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/first',
-      routes: {
-        '/first': (context) => const Screen1(),
-        '/second': (context) => const Screen2(name: 'Screen two'),
-        '/third': (context) => const Screen3(),
-      },
-      onGenerateRoute: (val) {
-        if (val.name == DynamicScreenTest.routeName) {
-          final dynamic args = val.arguments;
-          return MaterialPageRoute(
-            builder: (context) {
-              return DynamicScreenTest(args);
-            },
-          );
-        }
-      },
-      // home: DefaultTabController(
-      //   length: 3,
-      //   child: Scaffold(
-      //     appBar: AppBar(
-      //       backgroundColor: Colors.blueGrey,
-      //       bottom: const TabBar(
-      //         tabs: [
-      //           Tab(
-      //             text: "AC",
-      //             icon: Icon(Icons.ac_unit),
-      //           ),
-      //           Tab(icon: Icon(Icons.access_alarm)),
-      //           Tab(icon: Icon(Icons.zoom_out_sharp)),
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
+    return const MaterialApp(
+      home: Text("Hello.."),
     );
   }
 }
